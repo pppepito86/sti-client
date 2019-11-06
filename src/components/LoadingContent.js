1e16062d1abc785ec151591b106f9dfc2aadda70
Petar Petrov <pppepito86@gmail.com>
2019-11-07 00:29:27 +0200

    add spinner

--- src/components/LoadingContent.js
+++ src/components/LoadingContent.js
@@ -1,10 +1,14 @@
 import React from 'react';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faSpinner } from '@fortawesome/free-solid-svg-icons';
 
 const LoadingContent = () => {
   return (
     <div className="content-wrapper" style={{minHeight: '550px'}}>
       <section className="content">
-        Loading...
+        <div className="overlay fa-3x">
+          <FontAwesomeIcon icon={faSpinner} className="fa-spin" />
+        </div>
       </section>
     </div>
   )
