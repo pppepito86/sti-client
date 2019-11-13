2f45163fd4ae446397a5d7e47dc8ac5de5b13eb1
Petar Petrov <pppepito86@gmail.com>
2019-11-13 22:08:05 +0200

    fix verdict add key

--- src/components/Verdict.js
+++ src/components/Verdict.js
@@ -4,13 +4,13 @@ function Verdict({verdict}) {
   return (
     <div>
       {
-        verdict.slice().split(',').map((v) => {
+        verdict.slice().split(',').map((v, i) => {
           const color = v === 'OK' ? 'label-success' :
                         v === '?' ? 'label-default' :
                         v === 'waiting' || v === 'judging' ? 'label-success' :
                         'label-danger';
           const width = v === 'waiting' || v === 'judging' ? '50px' : '26px';
-          return <span className={`label ${color}`} 
+          return <span key={i} className={`label ${color}`} 
               style={{display: 'inline-block', 
               width: width, 
               marginRight: '1px'}}>
