55fa6538bdd6cda893a1b6c96328340d731cbb10
Petar Petrov <pppepito86@gmail.com>
2019-11-13 21:49:51 +0200

    increase verdict waiting/judging width

--- src/components/Verdict.js
+++ src/components/Verdict.js
@@ -5,13 +5,14 @@ function Verdict({verdict}) {
     <div>
       {
         verdict.slice().split(',').map((v) => {
-          const color = v==='OK' ? 'label-success' :
-                        v==='?' ? 'label-default' :
-                        v === 'waiting' || v==='judging' ? 'label-success' :
+          const color = v === 'OK' ? 'label-success' :
+                        v === '?' ? 'label-default' :
+                        v === 'waiting' || v === 'judging' ? 'label-success' :
                         'label-danger';
+          const width = v === 'waiting' || v === 'judging' ? '50px' : '26px';
           return <span className={`label ${color}`} 
               style={{display: 'inline-block', 
-              width: '26px', 
+              width: width, 
               marginRight: '1px'}}>
             {v}
           </span>
